f43178becdd773bb3863e934c9c300a4c3742282
FSD2025 <chriswakar@gmail.com>
2026-01-31 14:28:16 +0530

    Refactor authMiddleware to improve token handling

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,17 +1,19 @@
 import jwt from 'jsonwebtoken';
 
 export const authMiddleware = (req, res, next) => {
+ const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({ message: "No token provided" });
+  }
+
+  const token = authHeader.split(" ")[1];
+
   try {
-    const token = req.headers.authorization?.split(' ')[1];
-
-    if (!token) {
-      return res.status(401).json({ message: 'No token provided' });
-    }
-
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.userId = decoded.userId;
-    //next();
+    req.user = decoded; // attach user info
+    next();
   } catch (error) {
-    res.status(401).json({ message: 'Invalid or expired token' });
+    res.status(401).json({ message: "Invalid or expired token" });
   }
 };
